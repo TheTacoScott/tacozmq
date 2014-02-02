8d2e8d7811cb22c53e0e0ec9dc9cb616a0b69a51
Scott Powers <withorwithoutgod@gmail.com>
2014-02-02 14:38:19 -0600

    cleaned up taconet.py

--- taconet.py
+++ taconet.py
@@ -10,19 +10,19 @@ import sys
 import argparse
 import zmq
 import json
+import logging
+
 import taco.bottle
 import taco.constants
 import taco.server
 import taco.dispatch
 import taco.crypto
 import taco.settings
-import logging
 
-if __name__ == '__main__':
-  if zmq.zmq_version_info() < (4,0):
-    raise RuntimeError("Security is not supported in libzmq version < 4.0. libzmq version {0}".format(zmq.zmq_version()))
-  if sys.version_info < (2, 6, 5):
-    raise RuntimeError("must use python 2.6.6 or greater")
+if zmq.zmq_version_info() < (4,0):
+  raise RuntimeError("Security is not supported in libzmq version < 4.0. libzmq version {0}".format(zmq.zmq_version()))
+if sys.version_info < (2, 6, 5):
+  raise RuntimeError("must use python 2.6.6 or greater")
   
 parser = argparse.ArgumentParser(description='TacoNET: a darknet written in python and zeromq')
 parser.add_argument('--config', default=taco.constants.JSON_SETTINGS_FILENAME,dest='configfile',help='specify the location of the config json')
