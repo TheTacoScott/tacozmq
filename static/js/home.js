35ab59b1fbaec5a234f676e2e2d86fc47464a1f3
Scott Powers <withorwithoutgod@gmail.com>
2014-02-11 18:11:06 -0600

    Added better js timeouts for peer responsiveness foo

--- static/js/home.js
+++ static/js/home.js
@@ -11,23 +11,23 @@ function Update_Peer_Status()
         out = data[1];
         indiff = data[2];
         outdiff = data[3];
-        if (indiff >= 5.0) 
+        if (indiff >= 6.0) 
         { 
           $tr.find(".incomingstatus").find(".glyphicon").removeClass("glyphicon-question-sign glyphicon-ok-sign").addClass("glyphicon-minus-sign"); 
           $tr.find(".incomingstatus").removeClass("yellow-td green-td").addClass("red-td");
         }
-        else if (indiff < 5.0 && indiff >= 0.0) 
+        else if (indiff < 6.0 && indiff >= 0.0) 
         {
           $tr.find(".incomingstatus").find(".glyphicon").removeClass("glyphicon-question-sign glyphicon-minus-sign").addClass("glyphicon-ok-sign");
           $tr.find(".incomingstatus").removeClass("yellow-td red-td").addClass("green-td");
         }
 
-        if (outdiff >= 5.0)
+        if (outdiff >= 6.0)
         {
           $tr.find(".outgoingstatus").find(".glyphicon").removeClass("glyphicon-question-sign glyphicon-ok-sign").addClass("glyphicon-minus-sign");
           $tr.find(".outgoingstatus").removeClass("yellow-td green-td").addClass("red-td");
         }
-        else if (outdiff < 5.0 && outdiff >= 0.0)
+        else if (outdiff < 6.0 && outdiff >= 0.0)
         {
           $tr.find(".outgoingstatus").find(".glyphicon").removeClass("glyphicon-question-sign glyphicon-minus-sign").addClass("glyphicon-ok-sign");
           $tr.find(".outgoingstatus").removeClass("yellow-td red-td").addClass("green-td");
